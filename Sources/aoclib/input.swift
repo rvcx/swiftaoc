82b397f69747ae90f41f062cc14a712f6eb5f37f
Rob Shearer <rob@v.cx>
2021-11-30 14:55:22 -0700

    add functions for reading/parsing puzzle input

---
+++ Sources/aoclib/input.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public func lines(file: String = "input.txt") -> [Substring] {
+  return try! String(contentsOfFile: file).split(separator: "\n")
+}
+
+public func records(file: String = "input.txt") -> [[String]] {
+  let lines = try! String(contentsOfFile: file)
+                    .split(separator: "\n", omittingEmptySubsequences: false)
+  var curRecord = [String]()
+  var out = [[String]]()
+  for l in lines {
+    if l.isEmpty {
+      out.append(curRecord)
+      curRecord = [String]()
+    } else {
+      curRecord.append(String(l))
+    }
+  }
+  return out
+}
+
+public func parseLines(_ pattern: String,
+                       file: String = "input.txt") -> [[String?]] {
+  let re = try! NSRegularExpression(pattern: pattern)
+  var outs = [[String?]]()
+  for s in try! String(contentsOfFile: file).split(separator: "\n") {
+    let s = String(s)
+    let allS = NSRange(s.startIndex..<s.endIndex, in: s)
+    var out = [String?]()
+    let match = re.firstMatch(in: s, range: allS)!
+    for i in 1..<match.numberOfRanges {
+      let nsr = match.range(at: i)
+      if nsr.location != NSNotFound {
+        let g = s[Range(nsr, in: s)!]
+        out.append(String(g))
+      } else {
+        out.append(nil)
+      }
+    }
+    outs.append(out)
+  }
+  return outs
+}
+
---
+++ Tests/aoclibTests/inputTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import aoclib
+
+final class inputTests: XCTestCase {
+  func testLines() throws {
+    let nums = lines(file: "Tests/data/nums.txt").map { Int($0) }
+    XCTAssertEqual(nums, [2, -853, 1544])
+  }
+  func testRecords() throws {
+    let rs = records(file: "Tests/data/records.txt")
+    XCTAssertEqual(rs, [["Dave Waddler", "Engineer", "8899 West 82nd St"],
+                        ["Madeleine Enschly", "CEO"],
+                        ["Indigo the Bloodless", "The Wastelands"]])
+  }
+  func testParseLines() throws {
+    let ts = parseLines(#"(.*) (?:(borrowed)|(lent)|(gave)) \$(\d+) (?:from|to) (.*).$"#,
+                        file: "Tests/data/transactions.txt")
+    XCTAssertEqual(ts, [["Arthur Dent", "borrowed", nil, nil, "20", "Ford Prefect"],
+                        ["Zaphod", nil, "lent", nil, "8", "Marvin"],
+                        ["Trillian", nil, nil, "gave", "50", "Zaphod"]])
+    
+  }
+}
