08be935279319801f0e0af2529e0c7553645ed43
Rob Shearer <rob@v.cx>
2021-12-11 13:31:41 -0700

    add hack to make grid stuff work with arrays of strings

--- Sources/aoclib/Grid.swift
+++ Sources/aoclib/Grid.swift
@@ -81,3 +81,66 @@ extension Pt2 {
     return out
   }
 }
+
+// Ugh; duplicate the above for StringProtocols, which aren't
+// RandomAccessCollections...
+
+public struct SGridIterator<T>: Sequence, IteratorProtocol
+    where T: RandomAccessCollection, T.Index == Int,
+          T.Element: StringProtocol {
+
+  var p = Pt2(0, 0)
+  let c: T
+  public mutating func next() -> (Pt2, T.Element.Element)? {
+    guard p.y < c.count else { return nil }
+    defer {
+      p = Pt2(p.x + 1, p.y)
+      if p.x == c[p.y].count {
+        p = Pt2(0, p.y + 1)
+      }
+    }
+    return (p, c[p])
+  }
+}
+extension RandomAccessCollection
+    where Index == Int, Element: StringProtocol {
+  public subscript(pos: Pt2) -> Element.Element {
+    return self[pos.y][pos.x]
+  }
+  public func asGrid() -> SGridIterator<Self> {
+    return SGridIterator<Self>(c: self)
+  }
+}
+extension Pt2 {
+  public func neighbors<T: StringProtocol>(_ grid: [T]) -> [Pt2] {
+    var out = [Pt2]()
+    for y in (self.y - 1)...(self.y + 1) {
+      if y < 0 || y >= grid.count { continue }
+      for x in (self.x - 1)...(self.x + 1) {
+        if x < 0 || x >= grid[y].count { continue }
+        let newP = Pt2(x, y)
+        if newP != self { out.append(newP) }
+      }
+    }
+    return out
+  }
+  public func mneighbors<T: StringProtocol>(_ grid: [T]) -> [Pt2] {
+    var out = [Pt2]()
+    if x > 0 { out.append(Pt2(x - 1, y)) }
+    if y > 0 { out.append(Pt2(x, y - 1)) }
+    if x < grid[y].count - 1 { out.append(Pt2(x + 1, y)) }
+    if y < grid.count - 1 { out.append(Pt2(x, y + 1)) }
+    return out
+  }
+  public func tneighbors<T: StringProtocol>(_ grid: [T]) -> [Pt2] {
+    var out = [Pt2]()
+    for y in (self.y - 1)...(self.y + 1) {
+      for x in (self.x - 1)...(self.x + 1) {
+        let ny = (y + grid.count) % grid.count
+        let newP = Pt2((x + grid[ny].count) % grid[ny].count, ny)
+        if newP != self { out.append(newP) }
+      }
+    }
+    return out
+  }
+}
